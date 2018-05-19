332de65805348a0d67dcb15988c881e8b923ddea
SunMing <1229536801@qq.com>
2018-05-19 13:42:46 +0800

    first commit

---
+++ src/main/java/com/example/jingxiMall/JingxiMallApplication.java
@@ -0,0 +1,12 @@
+package com.example.jingxiMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingxiMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JingxiMallApplication.class, args);
+	}
+}
